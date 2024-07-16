5ca3d0b8961a5d9b258992a5ff961b7be779f2af
dhrumpatel <dhrumpatel007@gmail.com>
2024-07-16 18:04:16 +0530

    timeStamp

--- models/bookSchema.js
+++ models/bookSchema.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
-const bookSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-});
+const bookSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Book = mongoose.model("Book", bookSchema);
 module.exports = Book;
